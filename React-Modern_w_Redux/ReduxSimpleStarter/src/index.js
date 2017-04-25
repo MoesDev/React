22e5eebe11fd52eb3d876c4071ce2c3e5d6165fa
Monet <mpxluv@msn.com>
2017-04-25 10:53:22 -0500

    updated repo for fresh f1st commit

---
+++ React-Modern_w_Redux/ReduxSimpleStarter/src/index.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY ='AIzaSyDEfXbsAii8I5gUgTtDwMQR93O4rK0k-pk';
+
+
+
+// Create a new component.  This should produce some HTML
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {videos: []};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {this.setState({videos});
+});
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+// Take these component's generated in HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
