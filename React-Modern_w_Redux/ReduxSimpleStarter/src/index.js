74c838854637aace748ecc818c51d090cf378283
Monet <mpxluv@msn.com>
2017-04-27 16:36:05 -0500

    style&searchThrottle update

--- React-Modern_w_Redux/ReduxSimpleStarter/src/index.js
+++ React-Modern_w_Redux/ReduxSimpleStarter/src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY ='AIzaSyDEfXbsAii8I5gUgTtDwMQR93O4rK0k-pk';
 
@@ -14,16 +16,36 @@ class App extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = {videos: []};
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {this.setState({videos});
-});
+		this.videoSearch('macaron');
 	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term}, (videos) => 
+			{this.setState({
+				videos,
+				selectedVideo: videos[0]
+			});
+		});
+
+	}
+
+
 	render(){
+
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+					videos={this.state.videos} />
 			</div>
 		);
 	}
